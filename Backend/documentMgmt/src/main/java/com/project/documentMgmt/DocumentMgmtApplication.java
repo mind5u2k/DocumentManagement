af073e045931fbc9eace9779f714ab4de2a74a70
Anurag Ghosh <anurag.ghosh.1014@gmail.com>
2025-05-20 01:13:09 +0530

    Added base structure of Document Management project

---
+++ Backend/documentMgmt/src/main/java/com/project/documentMgmt/DocumentMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.project.documentMgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentMgmtApplication.class, args);
+	}
+
+}
---
+++ Backend/documentMgmt/src/test/java/com/project/documentMgmt/DocumentMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.documentMgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
